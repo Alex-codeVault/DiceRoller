a3893403b7ccf3037e9115b700ba0a4099f498b1
Alex Herrera <54720239+White-Hat-Blue-Feather@users.noreply.github.com>
2019-09-22 12:44:46 -0700

    Added an event action to roll_button when clicked (Toast).

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val Button: Button = findViewById(R.id.roll_button)
+        roll_button.text = "Let's roll"
+        roll_button.setOnClickListener { Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show() }
     }
 }
